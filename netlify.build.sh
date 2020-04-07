899956bb3081e815a7f963e97b7b40deba453c8d
Levon Kirakosyan <noosx.e@gmail.com>
2020-04-06 17:44:29 -0700

    Instruct curl to follow redirects.

--- netlify.build.sh
+++ netlify.build.sh
@@ -3,6 +3,7 @@
 set -e
 
 curl https://github.com/bazelbuild/bazel/releases/download/2.2.0/bazel-2.2.0-installer-linux-x86_64.sh \
+  -L \
   -o bazel_installer.sh
 chmod +x bazel_installer.sh
 ./bazel_installer.sh --user
