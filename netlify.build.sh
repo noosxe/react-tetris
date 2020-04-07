514ba818b1353a75dfbe2b6ef40a40eb58dacd4b
Levon Kirakosyan <noosx.e@gmail.com>
2020-04-06 17:37:35 -0700

    Fix netlify build command.

--- netlify.build.sh
+++ netlify.build.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-apt install curl
 curl https://github.com/bazelbuild/bazel/releases/download/2.2.0/bazel-2.2.0-installer-linux-x86_64.sh \
   -o bazel_installer.sh
 chmod +x bazel_installer.sh
