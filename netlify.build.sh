d89600ea737de3a83806c5a3ff06fc6b4ac44ab1
Levon Kirakosyan <noosx.e@gmail.com>
2020-04-06 17:01:50 -0700

    Add netlify build script.

---
+++ netlify.build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo apt install curl
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+
+sudo apt update && sudo apt install bazel
+
+yarn build
