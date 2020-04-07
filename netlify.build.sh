2fdb9a0d8c417f061dcda9d57accfe12adbfa6b7
Levon Kirakosyan <noosx.e@gmail.com>
2020-04-06 17:36:04 -0700

    Try using bazel binary installer for netlify build.

--- netlify.build.sh
+++ netlify.build.sh
@@ -3,9 +3,9 @@
 set -e
 
 apt install curl
-curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
-echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-
-apt update && apt install bazel
+curl https://github.com/bazelbuild/bazel/releases/download/2.2.0/bazel-2.2.0-installer-linux-x86_64.sh \
+  -o bazel_installer.sh
+chmod +x bazel_installer.sh
+./bazel_installer --user
 
 yarn build
