38f1c116f3959f2e6abc3ec802fb0f853c3436ed
Levon Kirakosyan <noosx.e@gmail.com>
2020-04-06 20:42:33 -0700

    Configure rollup for proper production build.

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+
+export default {
+  output: {
+    format: 'esm',
+  },
+  plugins: [
+    resolve(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    commonjs({
+      namedExports: {
+        // left-hand side can be an absolute path, a path
+        // relative to the current directory, or the name
+        // of a module in node_modules
+        'react': ['createElement'],
+        'react-dom': ['render'],
+      },
+    }),
+  ],
+};
